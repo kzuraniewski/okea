e49a52252a7c10e2aaadfc643970dc1583468a69
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2022-09-25 01:41:54 +0200

    global reset

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import GlobalStyle from "./globalStyle";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+	<React.StrictMode>
+		<GlobalStyle />
+		<App />
+	</React.StrictMode>
+);
